17a587f66b6efd1c05647a307584827d6c452386
jordyn-Steph <remmysmail@gmail.com>
2021-11-29 12:12:56 -0600

    moving things into ship class

--- main/sketch.js
+++ main/sketch.js
@@ -4,13 +4,11 @@
 
 
 // global variables
-let Bullets = [];
 let ship;
 let gameover = false;
 let x = 300;
 let y = 450;
 let speed = 4;
-let bulletSpeed = 1;
 let backgroundColor = "black";
 let ball;
 let lastChanged = 700;
@@ -28,14 +26,68 @@ class enemyShip {
   constructor(shipSprite){
     this.x = 300;
     this.y = 100;
+    this.bulletArray = [];
+    this.bulletSpeed = 1;
+    this.shipSpeed = 0;
     this.sprite = shipSprite;
   }
   display(){
     imageMode(CENTER);
     image(this.sprite,this.x,this.y,50,50);
+    for (let i = this.bulletArray.length-1; i > 0; i--) {
+      this.bulletArray[i].bulletUpdate();
+      this.bulletArray[i].show();
+    }
   }
   move(){
-    this.x += 0;
+    this.x += this.shipSpeed;
+  }
+  bullet8blast(){
+    for (let i = 0; i < 8; i ++) {
+      let bullet = new Bullet();
+      angleMode(DEGREES);
+      bullet.rotation = 360/8 * i + rotation;
+      bullet.x = ship.x;
+      bullet.y = ship.y;
+      bullet.dx = cos(bullet.rotation) * this.bulletSpeed * 2;
+      bullet.dy = sin(bullet.rotation) * this.bulletSpeed * 2;
+      // start doing the bullet spray
+      this.bulletArray.push(this.bullet);
+      console.log(bullet.x,bullet.y);
+    }
+  }
+  bulletshotgun3(){
+    for (let i = 0; i < 3; i ++) {
+      let bullet = new Bullet();
+      angleMode(DEGREES);
+      rotation += 20;
+      bullet.rotation = 10/3 * i + rotation;
+      bullet.x = ship.x;
+      bullet.y = ship.y;
+      bullet.dx = cos(bullet.rotation) * this.bulletSpeed * 2;
+      bullet.dy = sin(bullet.rotation) * this.bulletSpeed * 2;
+      // start doing the bullet spray
+      this.bulletArray.push(bullet);
+      console.log(bullet.x,bullet.y);
+      console.log("bulletpushed3");
+    }
+  }
+  randomDropdown1(){
+
+
+  }
+  spawnBullets(){
+    this.bullet8blast();
+    if(millis() > 1000){
+      this.bulletshotgun3();
+    }
+  }
+  bulletSpawnHandler() {
+    if (spawn === true) {
+      this.spawnBullets();
+      rotation += 60;
+      spawn = false;
+    }
   }
 }
 class Bullet {
@@ -55,7 +107,7 @@ class Bullet {
     rect(this.x,this.y,10,10);
     // pop();
   }
-  moveAndHitDet() {
+  bulletUpdate() {
     this.y += this.dy;
     this.x += this.dx;
     let hit = collideRectRect(this.x,this.y,10,10,x,y,80,5);
@@ -63,7 +115,9 @@ class Bullet {
       gotHit = true;
     }
   }
-
+  OnScreen() {
+    return this.y <= height && this.y >= 0 && this.x <= width && this.x >= 0;
+  }
 }
 
 //draws the canvas (the size is meant to be limited)
@@ -88,15 +142,9 @@ function draw() {
     // Bullets.pop();
     //}
     gotHit = false;
-    bulletSpeed = 1;
+    this.bulletSpeed = 1;
   }
-  bulletSpawnHandler();
-
-  //loops the bullets moving
-  for (let i = 0; i < Bullets.length; i++) {
-    Bullets[i].moveAndHitDet();
-    Bullets[i].show();
-  }
+  ship.bulletSpawnHandler();
 }
 
 //this is how the player being controlled is handled
@@ -122,60 +170,67 @@ function handleKeys() {
 }
 
 //handles bullets being spawned at reasonable rates unless the player lasts a while
-function bulletSpawnHandler() {
-  if (spawn === true) {
-    spawnBullets();
-    rotation += 60;
-    spawn = false;
-  }
-  if (millis() > 1000){
-    //   spawnBullets();
-    spawn = true;
-  }
-  //if (millis() > lastChanged){
-  //   lastChanged += 100 - bulletSpeed * 4;
-  //   spawn = true;
-  // console.log(lastChanged);
-  //}
-}
+// function bulletSpawnHandler() {
+//   if (spawn === true) {
+//     spawnBullets();
+//     rotation += 60;
+//     spawn = false;
+//   }
+//   if (millis() > 1000){
+//     //   spawnBullets();
+//     spawn = true;
+//   }
+//   if(millis() > 1008){
+//     spawn = false;
+//   }
+//   //if (millis() > lastChanged){
+//   //   lastChanged += 100 - bulletSpeed * 4;
+//   //   spawn = true;
+//   // console.log(lastChanged);
+//   //}
+// }
 
 //adds bullets to the array so they exist
-function spawnBullets(){
-  bullet8blast();
-  if(millis() > 1000){
-    bulletshotgun3();
-  }
-}
-function bullet8blast(){
-  for (let i = 0; i < 8; i ++) {
-    let bullet = new Bullet();
-    angleMode(DEGREES);
-    bullet.rotation = 360/8 * i + rotation;
-    bullet.x = ship.x;
-    bullet.y = ship.y;
-    bullet.dx = cos(bullet.rotation) * bulletSpeed * 2;
-    bullet.dy = sin(bullet.rotation) * bulletSpeed * 2;
-    // start doing the bullet spray
-    Bullets.push(bullet);
-    console.log(bullet.x,bullet.y);
-  }
-}
-function bulletshotgun3(){
-  for (let i = 0; i < 3; i ++) {
-    let bullet = new Bullet();
-    angleMode(DEGREES);
-    rotation += 20;
-    bullet.rotation = 10/3 * i + rotation;
-    bullet.x = ship.x;
-    bullet.y = ship.y;
-    bullet.dx = cos(bullet.rotation) * bulletSpeed * 2;
-    bullet.dy = sin(bullet.rotation) * bulletSpeed * 2;
-    // start doing the bullet spray
-    Bullets.push(bullet);
-    console.log(bullet.x,bullet.y);
-    console.log("bulletpushed3");
-  }
-}
+// function spawnBullets(){
+//   bullet8blast();
+//   if(millis() > 1000){
+//     bulletshotgun3();
+//   }
+// }
+// function bullet8blast(){
+//   for (let i = 0; i < 8; i ++) {
+//     let bullet = new Bullet();
+//     angleMode(DEGREES);
+//     bullet.rotation = 360/8 * i + rotation;
+//     bullet.x = ship.x;
+//     bullet.y = ship.y;
+//     bullet.dx = cos(bullet.rotation) * bulletSpeed * 2;
+//     bullet.dy = sin(bullet.rotation) * bulletSpeed * 2;
+//     // start doing the bullet spray
+//     Bullets.push(bullet);
+//     console.log(bullet.x,bullet.y);
+//   }
+// }
+// function bulletshotgun3(){
+//   for (let i = 0; i < 3; i ++) {
+//     let bullet = new Bullet();
+//     angleMode(DEGREES);
+//     rotation += 20;
+//     bullet.rotation = 10/3 * i + rotation;
+//     bullet.x = ship.x;
+//     bullet.y = ship.y;
+//     bullet.dx = cos(bullet.rotation) * bulletSpeed * 2;
+//     bullet.dy = sin(bullet.rotation) * bulletSpeed * 2;
+//     // start doing the bullet spray
+//     Bullets.push(bullet);
+//     console.log(bullet.x,bullet.y);
+//     console.log("bulletpushed3");
+//   }
+// }
+// function randomDropdown1(){
+//   ship.x = random(40,660);
+
+// }
 //displays player and my shape
 function displayEntities() {
   rect(x,y,80,5);
